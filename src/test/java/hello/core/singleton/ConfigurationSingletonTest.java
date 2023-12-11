abeefc7903cc342a7ac51cd3804bef71e8dc731f
hyejin1231 <phj981231@naver.com>
2023-12-11 11:03:13 +0900

    2023.12.11 섹션5. 싱글톤 컨테이너 - @Configuration과 싱글톤

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import static org.assertj.core.api.Assertions.*;
+
+// @Configuration 과 싱글톤
+public class ConfigurationSingletonTest
+{
+	@Test
+	void configurationTest()
+	{
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+		OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+		MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+		
+		MemberRepository memberRepository1 = memberService.getMemberRepository();
+		MemberRepository memberRepository2 = orderService.getMemberRepository();
+		
+		// 다 같은 memberRepository 인스턴스 객체임을 알 수 있다.
+		System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+		System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+		System.out.println("memberRepository = " + memberRepository);
+		
+		assertThat(memberRepository1).isSameAs(memberRepository2);
+		assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+		
+	}
+}
